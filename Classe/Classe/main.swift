ad8e7c33230f6f9db13f4f3c9db31346f8cc05fc
Felipe Augusto <augusto_felipe@icloud.com>
2023-03-08 20:45:00 -0300

    classes criadas

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -7,5 +7,58 @@
 
 import Foundation
 
-print("Hello, World!")
+import Foundation
+    
+class Impressora {
+    
+    var marca: String = "HP"
+    var modelo: String = "5550"
+    var quantidadePapel: Int = 1000
+    var temTinta: Bool = true
+    var temPapel: Bool = false
+    
+    
+    func imprimir() {
+        print("Impressora imprimindo.")
+        quantidadePapel -= 1
+    }
+    
+    func ligarImpressora() {
+        print("Impressora ligada.")
+    }
+}
 
+class Guitarra {
+    
+    var marca: String = "Fender"
+    var modelo: String = "Strato"
+    var madeira: String = "Maple"
+    var quantidadeCordas: Int = 6
+    var regulada: Bool = true
+    
+    
+    func tocarMusica() {
+        print("Tocando música.")
+    }
+    
+    func trocarCordas() {
+        print("Colocando cordas novas.")
+    }
+}
+
+class Mac {
+    
+    var modelo: String = "Mac Mini"
+    var ano: Int = 2020
+    var memoria: Int = 256
+    var bateria: Int = 99
+    var macOS: String = "Ventura"
+    
+    func estudarSwift() {
+        print("Estudando Swift.")
+    }
+    
+    func jogar() {
+        print("Jogando.")
+    }
+}
